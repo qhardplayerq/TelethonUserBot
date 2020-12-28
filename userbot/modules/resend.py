cd5123ea5ebfc32fec2ae4947349dd5f15df0325
qhardplayerq <62712377+qhardplayerq@users.noreply.github.com>
2020-12-28 20:16:04 +0200

    Update resend.py

--- userbot/modules/resend.py
+++ userbot/modules/resend.py
@@ -1,16 +1,8 @@
 # This Source Code Form is subject to the terms of the Mozilla Public
 # License, v. 2.0. If a copy of the MPL was not distributed with this
 # file, You can obtain one at http://mozilla.org/MPL/2.0/.
-import logging
-
 from telethon import events
-
 from userbot import bot
-
-logging.basicConfig(format='[%(levelname) 5s/%(asctime)s] %(name)s: %(message)s',
-                    level=logging.WARNING)
-logger = logging.getLogger(__name__)
-
 
 @bot.on(events.NewMessage(pattern=r"\.resend", outgoing=True))
 async def _(event):
@@ -18,4 +10,5 @@ async def _(event):
     m = await event.get_reply_message()
     if not m:
         return
+    await m.delete()
     await event.respond(m)
