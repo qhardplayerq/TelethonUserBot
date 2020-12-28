57d8218db8f6c6b8cc0d74370a0c26cb7a7ca6a5
qhardplayerq <62712377+qhardplayerq@users.noreply.github.com>
2020-12-28 19:13:29 +0200

    Create kisa.py

---
+++ userbot/modules/kisa.py
@@ -0,0 +1,33 @@
+import json
+import logging
+import requests
+# import aiohttp
+import asyncio
+from userbot import bot
+from userbot.util import admin_cmd
+from urllib.request import Request, urlopen
+logging.basicConfig(format='[%(levelname) 5s/%(asctime)s] %(name)s: %(message)s',
+                    level=logging.WARNING)
+logger = logging.getLogger(__name__)
+
+
+
+@bot.on(admin_cmd(pattern=("kisalt ?(.*)")))
+async def get_adzan(event):
+  link = event.pattern_match.group(1)
+  if link:
+      api = f"https://ay.link/api/?api=e2bb35a996ea8c9dfa4e5011005730bb584e283f&url={link}&alias&ct=1"
+      req = Request(api, headers={'User-Agent': 'Mozilla/5.0'})
+      web_byte = urlopen(req).read()
+      webpage = web_byte.decode('utf-8')
+      data = json.loads(webpage)
+      await event.edit(data['shortenedUrl'])
+
+
+
+
+
+
+
+        
+        
