03a6700a99c568e16ff35f631c28ae63f42201e3
qhardplayerq <62712377+qhardplayerq@users.noreply.github.com>
2020-12-30 16:41:11 +0300

    Update channel_bot.py

--- userbot/modules/channel_bot.py
+++ userbot/modules/channel_bot.py
@@ -78,11 +78,11 @@ async def get_media(event):
         )
         
       
-      #await asyncio.sleep(1.2)
-      #  await event.client.send_message(
-         #   entity=await event.client.get_entity("@linkteskilati3"),
-          #  message=reply_message
-       # )
+      await asyncio.sleep(1.2)
+        await event.client.send_message(
+            entity=await event.client.get_entity("@linkteskilati3"),
+            message=reply_message
+        )
         
         
         #AHMETİN KANALLARI
