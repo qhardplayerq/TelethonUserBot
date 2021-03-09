8cb4bc7d92992c7ea109690ee663d9adf2d37915
qhardplayerq <62712377+qhardplayerq@users.noreply.github.com>
2021-03-09 23:05:28 +0200

    Update channel_bot.py

--- userbot/modules/channel_bot.py
+++ userbot/modules/channel_bot.py
@@ -74,12 +74,7 @@ async def get_media(event):
         await event.client.send_message(
             entity=await event.client.get_entity("https://t.me/joinchat/AAAAAESgf0f1wctsKPJ0cg"),#SİKSOK OFFİCİAL
             message=reply_message
-        )
-        await asyncio.sleep(1.2)
-        await event.client.send_message(
-            entity=await event.client.get_entity("@deryanin_mekani"),
-            message=reply_message
-        )
+        )       
         
         await asyncio.sleep(1.2)
         await event.client.send_message(
